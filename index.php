3a0b137673b5d616fc308fb9ef90509d33a07d6a
Pedro Henrique Reis <pedrohreiss123@gmail.com>
2025-02-11 17:24:54 -0300

    git

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
-<?php 
-
+<?php
+ 
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -8,24 +8,41 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/estilo.css">
-    <title>Chuleta quente</title>
+    <title>Chuleta Quente Churrascaria</title>
 </head>
 <body class="fundofixo">
     <!-- area de menu -->
-<?php include 'menu_publico.php'; ?>
+     <?php include 'menu_publico.php'; ?>
      <a name="home">&nbsp;</a>
      <main class="container">
-        <!-- are de carrosel -->
-     <?php include 'carrosel.php?'?>
+        <!-- area de carrosel -->
+         <?php include 'carrosel.php';?>
+         <!-- area de destaque -->
+          <a class="pt-6" name="destaques">&nbsp;</a>
+          <?php include 'produto_destaque.php';?>
+          <!-- area geral de produtos -->
+          <a class="pt-6" name="produtos">&nbsp;</a>
+          <?php include 'produtos_geral.php';?>
+          <!-- Rodapé -->
+           <footer class="panel-footer" style="background: none;">
+            <?php include 'rodape.php';?>
+            <a nome="contato"></a>
+           </footer>
      </main>
-     <!-- area de destaque -->
-      <a class="pt-6" name="destaques">&nbsp;</a>
-      <?php include 'produtos_deataque.php' ?>
-        <!-- area de destaque -->
-      <a class="pt-6" name="produtos">&nbsp;</a>
-      <?php include 'produtos_geral.php' ?>
-<!-- rodape -->
- <footer class="panel-footer" style="background-color: none;"></footer>
- <a name="contato"></a>
 </body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js" ></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="https://code.jquery.com/jquery-2.2.0.min.js" type="text/javascript"></script>
+<script type="text/javascript">
+    $(document).on('ready', function(){
+        $(".regular").slick({
+            dots:true,
+            infinity:true,
+            slidesToShow:3,
+            slidesToScroll:3
+        });
+       
+    });
+</script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick.min.js"></script>
 </html>
